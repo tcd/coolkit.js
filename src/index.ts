f4c60b055d0761677484d25569e3c07a045c929d
Clay Dunston <claydunston@gmail.com>
2022-09-25 12:41:18 -0500

    [wip] publish esm & cjs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-export * from "./lib"
+export { default as isBlank } from "./is-blank"
+
+export { default as sleep } from "./sleep"
+
+export { default as updateArray } from "./update-array"
+export * from "./update-array"
