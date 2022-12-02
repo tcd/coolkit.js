a7fd2ea43ae6d792f12f6d66f4d06d064f676195
Clay Dunston <claydunston@gmail.com>
2022-12-02 09:46:12 -0600

    Add `writeJsonFileSync`

--- src/node/write-json-file-sync.ts
+++ src/node/write-json-file-sync.ts
@@ -1,6 +1,4 @@
 import { writeFileSync } from "fs"
-import { dirname } from "path"
-import mkdirp from "mkdirp"
 import { red, blue } from "colorette"
 
 /**
@@ -8,17 +6,13 @@ import { red, blue } from "colorette"
  *
  * Stringifies data.
  *
- * Makes parent folders for the path if they don't already exist.
- *
  * @param path Where to write out the file.
  * @param data Data to write to the file. Is passed to `JSON.stringify`.
  * @param pretty Pretty format JSON output.
  */
-const writeJsonFile = async (path: string, data: any, pretty = true): Promise<string | null> => {
+const writeJsonFileSync = (path: string, data: any, pretty = true): string | null => {
     try {
         const outData = pretty ? JSON.stringify(data, null, 4) : JSON.stringify(data)
-        const parentDir = dirname(path)
-        await mkdirp(parentDir)
         writeFileSync(path, outData)
         console.log(`file written: ${blue(path)}`)
         return path
@@ -28,4 +22,4 @@ const writeJsonFile = async (path: string, data: any, pretty = true): Promise<st
     }
 }
 
-export default writeJsonFile
+export default writeJsonFileSync
