d73b5470ee514a2d433940678823d308c25bab96
Clay Dunston <claydunston@gmail.com>
2023-01-19 08:42:57 -0600

    Update params for `writeJsonFile`

--- src/node/write-json-file.ts
+++ src/node/write-json-file.ts
@@ -3,6 +3,19 @@ import { dirname } from "path"
 import mkdirp from "mkdirp"
 import { red, blue } from "colorette"
 
+export type WriteJsonFileOptions = {
+    /**
+     * Pretty format JSON output.
+     * @default true
+     */
+    pretty?: boolean
+    /**
+     * Log out `path` after writing.
+     * @default true
+     */
+    log?: boolean
+}
+
 /**
  * Write `data` out to a file at the given `path` and log it.
  *
@@ -12,15 +25,21 @@ import { red, blue } from "colorette"
  *
  * @param path Where to write out the file.
  * @param data Data to write to the file. Is passed to `JSON.stringify`.
- * @param pretty Pretty format JSON output.
+ * @param options Additional options.
  */
-const writeJsonFile = async (path: string, data: any, pretty = true): Promise<string | null> => {
+const writeJsonFile = async (path: string, data: any, options: WriteJsonFileOptions = {}): Promise<string | null> => {
+    const {
+        pretty = true,
+        log = true,
+    } = options
     try {
         const outData = pretty ? JSON.stringify(data, null, 4) : JSON.stringify(data)
         const parentDir = dirname(path)
         await mkdirp(parentDir)
         writeFileSync(path, outData)
-        console.log(`file written: ${blue(path)}`)
+        if (log) {
+            console.log(`file written: ${blue(path)}`)
+        }
         return path
     } catch (error) {
         console.error(red(error.message))
